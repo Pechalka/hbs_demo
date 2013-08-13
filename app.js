ba5d3f18b5299b49d616a28e4126370e5a684b8a
Andrew <luzkovskyandrey@gmail.com>
2013-08-13 10:10:16 +0300

    add production mode - npm start

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var express = require('express')
 
 var app = express();
 
+
+ var ECT = require('ect');
+ var ectRenderer = ECT({ watch: true, root: path.join(__dirname, 'views') });
+ app.engine('.html', ectRenderer.render);
+ app.set('view engine', 'html');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -17,6 +23,9 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', function(req, res){
+	res.render('index', { is_production : process.argv[2] == 'production'}); // node app.js production
+})
 
 http.createServer(app).listen(3000, function(){
   console.log('Express server listening on port 3000');
